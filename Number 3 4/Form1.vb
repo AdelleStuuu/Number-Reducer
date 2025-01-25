aa1b1fa2b9f1b96d2dc4e0917187db8037911459
AdelleStuuu <adelledoesstuff@gmail.com>
2025-01-25 08:36:51 +0800

    Add project files.

---
+++ Number 3 4/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Btnsubmit_click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intNumber As Integer = NumericUpDown1.Value
+        Dim intNumberDisplay As Integer
+
+        intNumberDisplay = intNumber
+
+        While intNumber <> 1
+            intNumber -= 1
+        End While
+
+        MsgBox(intNumberDisplay & " is reduced to " & intNumber)
+    End Sub
+End Class
